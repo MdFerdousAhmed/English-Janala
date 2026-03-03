647c09250a4acb1bc116488d7b6ce7f3268e1a7c
MD FERDOUS AHMED <148644652+MdFerdousAhmed@users.noreply.github.com>
2026-03-03 19:08:34 +0600

    four commit

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
 
 
 
-const synonyms = ["hello","hi","konnichiwa"]
-createElements(synonyms);
+// const synonyms = ["hello","hi","konnichiwa"]
+// createElements(synonyms);
+
+const word = "Hello"
+console.log(`The word is:${word}`);
